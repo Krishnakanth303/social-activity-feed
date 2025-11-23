fe293c6202d239d5df9e83c8e39a9501a77ef95e
Krishnakanth303 <krishnakanthurs@gmail.com>
2025-11-23 11:58:52 +0530

    Resolve conflicts

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,6 @@
 from app.main import app
 
-# Entry point for running with: python app.py
+# Entry point for running with: python main.py
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8000)
